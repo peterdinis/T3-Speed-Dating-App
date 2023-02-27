0ac009be66ba8d0f50fb9cc0a8bbda4919325d5f
Peterdinis <peterdinis0@pobox.sk>
2023-02-27 14:16:55 +0100

    feat: FOO

---
+++ prisma/migrations/20230227131556_new_table_app_user/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "AppUser" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "contact" TEXT NOT NULL,
+
+    CONSTRAINT "AppUser_pkey" PRIMARY KEY ("id")
+);
